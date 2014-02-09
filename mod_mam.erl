b2de37c89e544ebafb809cb60751eaf195c6ed3a
kongo2002 <kongo2002@googlemail.com>
2014-02-09 22:34:41 +0100

    add license header

--- mod_mam.erl
+++ mod_mam.erl
@@ -1,3 +1,26 @@
+%%%----------------------------------------------------------------------
+%%% File    : mod_mam.erl
+%%% Author  : Gregor Uhlenheuer <kongo2002@gmail.com>
+%%% Purpose : Message Archive Management (XEP-0313)
+%%% Created : 29 Jan 2014 by Gregor Uhlenheuer <kongo2002@gmail.com>
+%%%
+%%%
+%%% This program is free software; you can redistribute it and/or
+%%% modify it under the terms of the GNU General Public License as
+%%% published by the Free Software Foundation; either version 2 of the
+%%% License, or (at your option) any later version.
+%%%
+%%% This program is distributed in the hope that it will be useful,
+%%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%%% General Public License for more details.
+%%%
+%%% You should have received a copy of the GNU General Public License
+%%% along with this program; if not, write to the Free Software
+%%% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+%%% 02111-1307 USA
+%%%----------------------------------------------------------------------
+
 -module(mod_mam).
 -author('kongo2002@googlemail.com').
 
